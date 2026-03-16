5257d0e4b5e6273b1643bdcab3cd228ff727c904
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-16 16:00:10 +0530

    update

--- variables and data types/operators.py
+++ variables and data types/operators.py
@@ -10,10 +10,19 @@ b = 5
 # print(a ** b) # exponentiation
 
 #relational operators
-print(a == b) # equal to
-print(a != b) # not equal to
-print(a > b) # greater than
-print(a < b) # less than    
-print(a >= b) # greater than or equal to
-print(a <= b) # less than or equal to   
+# print(a == b) # equal to
+# print(a != b) # not equal to
+# print(a > b) # greater than
+# print(a < b) # less than    
+# print(a >= b) # greater than or equal to
+# print(a <= b) # less than or equal to   
 
+#assingment operator
+# num = 10
+# num += 5 # num = num + 5
+# print(num) # 15
+
+#logical operators
+print(not False)
+print(True and False)
+print(True or False)
