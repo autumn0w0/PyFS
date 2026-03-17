89a74e7296ece035d0b1bcaaee6b998778c56cf0
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-17 17:41:52 +0530

    update

---
+++ ch2 string and cond statements/cond_state.py
@@ -0,0 +1,21 @@
+#example 1
+# light = str(input("enter the light: "))
+# if light == "red":
+#     print("stop")
+# elif light == "yellow":
+#     print("wait")
+# elif light == "green":
+#     print("go")
+# else:
+#     print("invalid light")
+
+#example 2
+marks = int(input("enter your marks: "))
+if marks >= 90:
+    print("grade A")
+elif marks >= 80:
+    print("grade B")
+elif marks >= 70:
+    print("grade C")
+else:
+    print("grade D")
