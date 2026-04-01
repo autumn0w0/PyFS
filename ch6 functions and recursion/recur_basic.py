493adb9114857d515b61dc8f804328eb5770e033
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-04-01 16:18:46 +0530

    fix

--- ch6 functions and recursion/recur_basic.py
+++ ch6 functions and recursion/recur_basic.py
@@ -1,5 +1,14 @@
 def show(n):
-    if(n==0):
+    if(n==0):  #base case
         return
     print(n)
     show(n-1)
+show(5)
+
+#factorial using recursion
+def fact(n):
+    if(n==0 or n==1):  #base case
+        return 1
+    return n * fact(n-1)
+
+print(fact(5))
