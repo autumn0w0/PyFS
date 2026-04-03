bef920e284d76c3b112667a67fa4ab4e61b7777f
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-04-03 17:08:16 +0530

    fix

--- ch8 oop/basic.py
+++ ch8 oop/basic.py
@@ -4,9 +4,19 @@
 # s1 = Student()
 # print(s1.name)  # autumn 
 
+# class Student:
+#     def __init__(self, fullname):
+#         self.name = fullname
+
+# s1 = Student("autumn")
+# print(s1.name)  # autumn
+
+#del keyword
 class Student:
     def __init__(self, fullname):
         self.name = fullname
 
 s1 = Student("autumn")
-print(s1.name)  # autumn
+print(s1.name)
+del s1
+# print(s1.name)  # NameError: name 's1' is not defined
